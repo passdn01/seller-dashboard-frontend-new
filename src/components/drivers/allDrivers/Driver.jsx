5124674fe83aa699694b8f356495abe48a5af017
sakshii00 <onlycodingdot@gmail.com>
2024-09-21 18:44:01 +0530

    driver card on driver page

--- src/components/drivers/allDrivers/Driver.jsx
+++ src/components/drivers/allDrivers/Driver.jsx
@@ -1,21 +1,28 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import SideNavbar from '../../SideNavbar';
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Button } from "@/components/ui/button"
+import backArrow from '../../../assets/backArrow.svg'
+import DriverCard from './DriverCard.jsx';
 
+
+import Header from './Header';
 
 function Driver() {
     const { id } = useParams();  // Destructure the id param
     const [data, setData] = useState({});
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get(`https://bhk8mp0s-2011.inc1.devtunnels.ms/dashboard/api/${id}`)
@@ -41,22 +48,56 @@ function Driver() {
         return <div>Error: {error}</div>;
     }
 
+
+
     return (
-        <div>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Card Title</CardTitle>
-                    <CardDescription>Card Description</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <p>Card Content</p>
-                </CardContent>
-                <CardFooter>
-                    <p>Card Footer</p>
-                </CardFooter>
-            </Card>
+        <div className='flex'>
+            <SideNavbar />
+            <div className='flex-1 ml-[250px]'>
+                <Header className='w-full' title='ALL DRIVERS' />
+                <div className='overflow-auto mx-8'>
+                    <div className='p-4 my-4 justify-between flex'>
+                        <Button variant='outline' className='shadow' onClick={() => {
+
+                            navigate('/drivers/allDrivers', { state: { fromBackButton: true } });
+                        }}><img src={backArrow} alt="" /></Button>
+                        <Button variant='outline' className='shadow text-blue-500' onClick={() => { return window.location.reload(); }}>REFRESH</Button>
+                    </div>
+                    <Breadcrumb className='px-4'>
+                        <BreadcrumbList>
+                            <BreadcrumbItem>
+                                <BreadcrumbLink href="/drivers/allDrivers" className='text-blue-500' >AllDrivers</BreadcrumbLink>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator />
+                            <BreadcrumbItem>
+                                <BreadcrumbLink >DriverDetail</BreadcrumbLink>
+                            </BreadcrumbItem>
+                        </BreadcrumbList>
+                    </Breadcrumb>
+                    <div>
+                        <div>
+                            <DriverCard data={data}></DriverCard>
+                        </div>
+                        <div className='flex'>
+                            rc and driving license <div></div>
+                            <div></div>
+                        </div>
+                        <div>
+                            performance
+                        </div>
+                        <div>
+                            charts
+                        </div>
+                        <div>
+                            subscription
+                        </div>
+                    </div>
 
 
+
+
+                </div>
+            </div>
         </div>
     );
 }
---
+++ src/components/drivers/allDrivers/DriverCard.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Card, CardContent, CardHeader, CardTitle, CardDescription, CardFooter } from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import referBy from '../../../assets/referBy.svg'
+const DriverCard = ({ data }) => {
+    const { driverInfo } = data;
+
+    return (
+        <>
+
+            <Card className='flex justify-between items-center px-2 my-4 mx-3'>
+                <div>
+                    <CardHeader className=' '>
+                        <div className='flex gap-x-4'>
+                            <Avatar>
+                                <AvatarImage src={driverInfo.profileUrl} alt={driverInfo.name} />
+                                <AvatarFallback>{driverInfo.name.charAt(0)}</AvatarFallback>
+                            </Avatar>
+
+                            <div>
+                                <CardTitle>{driverInfo.name}</CardTitle>
+                                <CardDescription>
+                                    {driverInfo.phone} <br />    Joined: {new Date(driverInfo.createdAt).toLocaleDateString('en-US', {
+                                        year: 'numeric',
+                                        month: 'long',
+                                        day: 'numeric'
+                                    })}
+                                </CardDescription>
+                            </div>
+                        </div>
+                    </CardHeader>
+                </div>
+                <div className='flex-col flex gap-y-1 pr-4'>
+                    <Badge className='bg-[#5356FF] text-white'>{driverInfo.status}</Badge>
+                    <div className='flex text-xs gap-1'><img src={referBy} alt="" />{driverInfo?.refferedBy || 'none'}</div>
+                </div>
+            </Card>
+        </>
+
+    );
+};
+
+export default DriverCard;
