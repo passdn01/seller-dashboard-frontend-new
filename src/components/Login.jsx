e0fdff6c5805ff3006976f063dcf68d86ef4cbd4
Devang <devanggandhi2222@gmail.com>
2024-12-30 10:45:01 +0530

    welcome devang

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
                
                 console.log(result);    
                 if (result.success === true) {
-                    alert("Logged in successfully"+" " +admin);
+                    alert("Welcome"+" " +admin);
                     window.location.href = '/home';
                 } else {
                     alert(result.message);
